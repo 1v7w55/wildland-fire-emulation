8ae90676583602a9b989ffd7e168d79310fc1912
Sacha <sachacmoi77.2@gmail.com>
2023-08-31 15:45:05 +0200

    code refactor + work in progress

--- reading.c
+++ reading.c
@@ -1,66 +1,36 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <stdio.h>
 #include <stdlib.h>
 
-#define bool unsigned int
-
-struct element {
-    const char caractère;
-    const short unsigned degre;
-    bool etat;
-};
-
-
-
-int main()
-{
-    int l,L;
-    //l=ligne L=colonne
-    scanf("%d %d",&l,&L);
-    
-
-    
-    
+int main() {
+    int height, width;
+    scanf("%d %d",&height,&width);
     char** tab = NULL;
+    tab = (char **) malloc(height * sizeof(char*));
+    if(tab == NULL) {
+        return -1;
+    }
     
-    tab = (char **) malloc( l * sizeof(char*) );
-    
-    if(tab == NULL) return -1;
-    
-    
-    
-    for(int i=0;i<l;i++){
-        
-        char* ligne=NULL;
-        ligne = (char *) malloc( (L+1) * sizeof(char*) );
-        
-        if(ligne == NULL) {
+    for(int i = 0;i < height;i++) {
+        char* line = NULL;
+        line = (char *) malloc((width+1) * sizeof(char*));
+        if(line == NULL) {
             free(tab);
             return -2;
         }
-        scanf("%s",ligne);
-        *(tab+i)=ligne;
-        
-    }
- 
-    printf("\n");
 
-    for(int i=0;i<l;i++){
-        for (int j=0;j<L;j++){
-            //printf("Tab[%d][%d] = %c\n", i, j, *(*(tab + i) + j));
-            printf("%c",*(*(tab + i) + j));
-            
-        }
-        printf("\n");
-        
+        scanf("%s", line);
+        *(tab + i) = line;
     }
+    printf("fin");
+    for(int i = 0;i < height;i++) {
+        for (int j = 0;j < width;j++){
+            //printf("tableau[%d][%d] = %c\n", i, j, *(*(tab + i) + j));
+            printf("%c", *(*(tab + i) + j));
+        }
+
+        printf("\n");
+    }
+
     return 0;
 }
 
