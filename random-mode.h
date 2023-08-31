712d98ed0ff09143cf98ce734d5b15e90dee0d6d
Serine93 <sisi.bettayeeb@gmail.com>
2023-08-31 16:27:01 +0200

    refacto

---
+++ random-mode.h
@@ -0,0 +1,17 @@
+#ifndef FOREST_H
+#define FOREST_H
+#pragma once
+#include <stddef.h>
+#include <time.h>
+
+
+
+
+struct Element {
+  char type;
+  char symbol;
+  int degree;
+  int state;
+};
+
+#endif 
