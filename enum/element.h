ea431df65b0500314f409133ee212a06f2cd017a
dvergoz <vergoz@et.esiea.fr>
2023-09-01 10:04:53 +0200

    adding enum

---
+++ enum/element.h
@@ -0,0 +1,37 @@
+#include "../bean/element.h"
+
+// Creation of an enum for faster access to random generation
+typedef enum {
+  GROUND,
+  TREE,
+  LEAF,
+  ROCK,
+  WATER,
+  HERB,
+  ASH,
+  EXTINCTASH
+} ElementEnum;
+
+// Element return based on the element in the enum
+Element getElementFromEnum(ElementEnum elementEnum) {
+  switch (elementEnum) {
+    case GROUND:
+      return ground;
+    case TREE:
+      return tree;
+    case LEAF:
+      return leaf;
+    case ROCK:
+      return rock;
+    case WATER:
+      return water;
+    case HERB:
+      return herb;
+    case ASH:
+      return ash;
+    case EXTINCTASH:
+      return extinctAsh;
+    default:
+      return ground;
+  }
+}
--- main.c
+++ main.c
@@ -3,49 +3,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#include "./config/global.h"
-#include "./bean/element.h"
-
-// Initialize values for individual elements
-static struct Element elements[] = {
-  // Floor
-  {'F', '+', 0, 0},
-  // Tree
-  {'T', '*', 4, 0},
-  // Leaf
-  {'L', ' ', 2, 0},
-  // Rock
-  {'R', '#', 5, 0},
-  // Herb
-  {'H', 'x', 3, 0},
-  // Water
-  {'W', '/', 0, 0},
-  // Ash
-  {'A', '-', 1, 0},
-  // Extinct ash
-  {'E', '@', 0, 0}
-};
-
-/**
- * This program generates a random width and height for an output,
- * and then prints a random symbol from an array for each row and column of the output.
- */
+#include "./display/random/random_display.h"
 
 int main() {
-  srand(time(NULL)); 
-
-  int randomWidth = (rand() % MAX_WIDTH) + MIN_WIDTH;
-  int randomHeight = (rand() % MAX_WIDTH) + MIN_WIDTH;
-
-  printf("Width: %d, Height: %d\n", randomWidth, randomHeight);
-
-  for (int i = 0; i < randomHeight; i++) {
-    for (int j = 0; j < randomWidth; j++) {
-      struct Element element = elements[rand() % (sizeof elements / sizeof elements[0])];
-      printf("%c", element.symbol);
-    }
-    printf("\n");
-  }
-
+  randomDisplay();
   return 0;
 }
