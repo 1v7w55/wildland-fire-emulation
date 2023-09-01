64a2c21ca01cd8c46c2c32fd037b731893ce5e7c
dvergoz <vergoz@et.esiea.fr>
2023-09-01 13:26:28 +0200

    adding allocate memory w/ malloc

--- config/global.h
+++ config/global.h
@@ -3,3 +3,6 @@ const unsigned short MAX_WIDTH = 10;
 const unsigned short MIN_WIDTH = 4;
 const unsigned short MAX_HEIGHT = 10;
 const unsigned short MIN_HEIGHT = 4;
+
+// Define spacer
+const char SPACER[] = "  ";
--- display/random/random_display.h
+++ display/random/random_display.h
@@ -12,18 +12,15 @@ void displayMatrix(Element **matrix, int randomWidth, int randomHeight, short st
     for (int j = 0; j < randomWidth; j++) {
       printf("%c", matrix[i][j].symbol);
     }
-    printf("   ");
+    printf("%s",SPACER);
     for (int j = 0; j < randomWidth; j++) {
-      if (matrix[i][j].degree >= 1) {
+      if (matrix[i][j].degree > 1) {
         matrix[i][j].degree -= 1;
       } else if (matrix[i][j].degree == 1) {
         matrix[i][j] = ash;
       } else if (
-        matrix[i][j].degree == 0 
-        && matrix[i][j].type != 'G' 
-        && matrix[i][j].type != 'W' 
-        && matrix[i][j].type != 'E'
-      ) {
+        matrix[i][j].degree == 0
+      ) else {
         matrix[i][j] = extinctAsh;
       }
       printf("%d", matrix[i][j].degree);
@@ -33,6 +30,23 @@ void displayMatrix(Element **matrix, int randomWidth, int randomHeight, short st
   printf("\n");
 }
 
+void initializeMatrix(Element **matrix, int randomWidth, int randomHeight) {
+  for (int i = 0; i < randomHeight; i++) {
+    matrix[i] = malloc(randomWidth * sizeof(Element));
+  }
+  for (int i = 0; i < randomHeight; i++) {
+    for (int j = 0; j < randomWidth; j++) {
+      Element element = getElementFromEnum(rand() % 5);
+      matrix[i][j] = element;
+    }
+  }
+}
+
+void getRandomPosition(int *randomX, int *randomY, int randomWidth, int randomHeight) {
+  *randomX = rand() % randomWidth;
+  *randomY = rand() % randomHeight;
+}
+
 int randomDisplay() {
   // time is used to generate a different number each time we run the program, if we didnt use it, we would get the same number each time
   srand(time(NULL));
@@ -40,24 +54,17 @@ int randomDisplay() {
   int randomHeight = (rand() % MAX_WIDTH) + MIN_WIDTH;
   // To debug only
   printf("Width: %d, Height: %d\n", randomWidth, randomHeight);
-  // Create matrix with random width and height
-  Element **matrix = malloc(randomHeight * sizeof(Element *));
-  for (int i = 0; i < randomHeight; i++) {
-    matrix[i] = malloc(randomWidth * sizeof(Element));
+  Element **forestMatrix = malloc(randomHeight * sizeof(Element *));
+  initializeMatrix(forestMatrix, randomWidth, randomHeight);
+  displayMatrix(forestMatrix, randomWidth, randomHeight, 0);
+
+  int randomX = 0;
+  int randomY = 0;
+  while (forestMatrix[randomY][randomX].degree == 0) {
+    getRandomPosition(&randomX, &randomY, randomWidth, randomHeight);
   }
+  forestMatrix[randomY][randomX].state = 1;
   
-  for (int i = 0; i < randomHeight; i++) {
-    for (int j = 0; j < randomWidth; j++) {
-      Element element = getElementFromEnum(rand() % 5);
-      matrix[i][j] = element;
-    }
-  }
-  // while some matrix[i][j].degree > 0, display matrix
-  short step = 0;
-  while (step < 6) {
-    displayMatrix(matrix, randomWidth, randomHeight, step);
-    step++;
-  }
-  free(matrix);
+  free(forestMatrix);
   return 0;
 }
