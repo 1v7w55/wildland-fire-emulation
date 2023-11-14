1f8377a13217c6577b3fc8d30ab28cce0851c279
Serine93 <sisi.bettayeeb@gmail.com>
2023-11-14 14:40:38 +0100

    Mise en place de la revision de la partie

--- display/random/random_display.c
+++ display/random/random_display.c
@@ -1,6 +1,3 @@
-
-
-
 #include "../../config/global.h"
 #include "../../config/error_message.h"
 #include "../../bean/type_element.h"
@@ -16,114 +13,175 @@
  
 // Freeing up memory space of the matrix
 void freeMatrix(Element** matrix, size_t height) {
-  for (size_t i = 0; i < height; i++) {
-    free(matrix[i]);
-  }
-  free(matrix);
+    for (size_t i = 0; i < height; i++) {
+        free(matrix[i]);
+    }
+    free(matrix);
 }
 
 void displayMatrix(Element** matrix, size_t width, size_t height) {
-  for (size_t i = 0; i < height; i++) {
-    for (size_t j = 0; j < width; j++) {
-      printf("%c", matrix[i][j].symbol);
+    for (size_t i = 0; i < height; i++) {
+        for (size_t j = 0; j < width; j++) {
+            printf("%c", matrix[i][j].symbol);
+        }
+        // DEBUG ONLY
+        printf("%s", SPACER);
+        for (size_t j = 0; j < width; j++) {
+            printf("%d", matrix[i][j].degree);
+        }
+        printf("%s", SPACER);
+        for (size_t j = 0; j < width; j++) {
+            printf("%d", matrix[i][j].state);
+        }
+        // END DEBUG
+        printf("\n");
     }
     // DEBUG ONLY
-    printf("%s", SPACER);
-    for (size_t j = 0; j < width; j++) {
-      printf("%d", matrix[i][j].degree);
-    }
-    printf("%s", SPACER);
-    for (size_t j = 0; j < width; j++) {
-      printf("%d", matrix[i][j].state);
-    }
+    printf("Etape : %ld\n", step);
+    step++;
     // END DEBUG
     printf("\n");
-  }
-  // DEBUG ONLY
-  printf("Etape : %ld\n", step);
-  step++;
-  // END DEBUG
-  printf("\n");
 }
 
 void initializeMatrix(Element** matrix, size_t width, size_t height) {
-  for (size_t i = 0; i < height; i++) {
-    matrix[i] = malloc(width * sizeof(Element));
-    if (!matrix[i]) {
-      fprintf(stderr, ERROR_MEMORY);
-      freeMatrix(matrix, i);
-      exit(1);
+    for (size_t i = 0; i < height; i++) {
+        matrix[i] = malloc(width * sizeof(Element));
+        if (!matrix[i]) {
+            fprintf(stderr, ERROR_MEMORY);
+            freeMatrix(matrix, i);
+            exit(1);
+        }
     }
-  }
-  for (size_t i = 0; i < height; i++) {
-    for (size_t j = 0; j < width; j++) {
-      Element element = getElementFromEnum(rand() % 5);
-      matrix[i][j] = element;
+    for (size_t i = 0; i < height; i++) {
+        for (size_t j = 0; j < width; j++) {
+            Element element = getElementFromEnum(rand() % 5);
+            matrix[i][j] = element;
+        }
     }
-  }
 }
 
-// TODO : issue with size_t so we use unsigned short
+// TODO: issue with size_t so we use unsigned short
 void getRandomPosition(unsigned short* randomX, unsigned short* randomY, unsigned short width, unsigned short height) {
-  *randomX = rand() % width;
-  *randomY = rand() % height;
+    *randomX = rand() % width;
+    *randomY = rand() % height;
 }
 
 // Find a way to avoid this function and set this into setFire
 void initFire(int randomX, int randomY, Element** forestMatrix) {
-  forestMatrix[randomY][randomX].state += 1;
-  forestMatrix[randomY][randomX].symbol = 'F';
+    forestMatrix[randomY][randomX].state += 1;
+    forestMatrix[randomY][randomX].symbol = 'F';
 }
 
 void setFire(int randomX, int randomY, size_t width, size_t height, Element** forestMatrix, Point* points, size_t* pointIndex) {
-  for (int i = -1; i <= 1; i++) {
-    for (int j = -1; j <= 1; j++) {
-      if (randomY + i >= 0 && randomY + i < height && randomX + j >= 0 && randomX + j < width &&
-        forestMatrix[randomY + i][randomX + j].state != 1 &&
-        forestMatrix[randomY + i][randomX + j].degree > 0) {
-        forestMatrix[randomY + i][randomX + j].state += 1;
-        forestMatrix[randomY + i][randomX + j].symbol = 'F';
-        points[*pointIndex].x = randomX + j;
-        points[*pointIndex].y = randomY + i;
-        (*pointIndex)++;
-      }
+    for (int i = -1; i <= 1; i++) {
+        for (int j = -1; j <= 1; j++) {
+            if (randomY + i >= 0 && randomY + i < height && randomX + j >= 0 && randomX + j < width &&
+                forestMatrix[randomY + i][randomX + j].state != 1 &&
+                forestMatrix[randomY + i][randomX + j].degree > 0) {
+                forestMatrix[randomY + i][randomX + j].state += 1;
+                forestMatrix[randomY + i][randomX + j].symbol = 'F';
+                points[*pointIndex].x = randomX + j;
+                points[*pointIndex].y = randomY + i;
+                (*pointIndex)++;
+            }
+        }
     }
-  }
 }
 
-int randomDisplay() {
-  srand(time(NULL));
-  size_t randomWidth = (rand() % (MAX_WIDTH - MIN_WIDTH + 1)) + MIN_WIDTH;
-  size_t randomHeight = (rand() % (MAX_WIDTH - MIN_WIDTH + 1)) + MIN_WIDTH;
-  Point* points = (Point*)malloc(sizeof(Point) * randomHeight * randomWidth);
-  if (!points) {
-    fprintf(stderr, ERROR_MEMORY);
-    return 1;
-  }
-  Element** forestMatrix = (Element**)malloc(randomHeight * sizeof(Element*));
-  if (!forestMatrix) {
-    fprintf(stderr, ERROR_MEMORY);
-    free(points);
-    return 1;
-  }
-  initializeMatrix(forestMatrix, randomWidth, randomHeight);
-  // We don't want to start the fire on a element with degree 0, so we loop until we find one
-  do {
-      getRandomPosition(&randomX, &randomY, randomWidth, randomHeight);
-  } while (forestMatrix[randomY][randomX].degree == 0);
-  // TODO: We need to display this first time to see the first step of the fire, change to only call one function
-  displayMatrix(forestMatrix, randomWidth, randomHeight);
-  // we set the fire on the random position
-  initFire(randomX, randomY, forestMatrix);
-  // TODO: Same as above, we need to display this first time to see the first step of the fire
-  displayMatrix(forestMatrix, randomWidth, randomHeight);
-  setFire(randomX, randomY, randomWidth, randomHeight, forestMatrix, points, &pointIndex);
-  // For each point in the array, we set the fire on the point and we add the new points to the array
-  for (size_t i = 0; i < pointIndex; i++) {
+int askToContinue() {
+    char choice;
+    printf("Vous voulez continuer? (o/n): ");
+    scanf(" %c", &choice);
+
+    return (choice == 'o' || choice == 'O');
+}
+
+
+void revoirEtapes(Element** forestMatrix, size_t width, size_t height) {
+    int choixRevoir;
+    printf("Voulez-vous revoir votre partie ?\n");
+    printf("1 - Oui\n2 - Non\n");
+    scanf("%d", &choixRevoir);
+
+    if (choixRevoir == 1) {
+        int moment;
+        printf("Quel moment ?\n");
+        printf("1 - Je veux tout revoir\n2 - Seulement une partie\n");
+        scanf("%d", &moment);
+
+        if (moment == 1) {
+            // Afficher toutes les étapes
+            for (size_t i = 0; i < pointIndex; i++) {
+                displayMatrix(forestMatrix, width, height);
+            }
+        } else if (moment == 2) {
+            int partie;
+            printf("Quelle partie ?\n");
+            printf("0, Partie 0\n1, Partie 1\n");
+            scanf("%d", &partie);
+
+            if (partie == 0 || partie == 1) {
+                // Afficher la partie spécifiée
+                displayMatrix(forestMatrix, width, height);
+            } else {
+                printf("Option invalide.\n");
+            }
+        } else {
+            printf("Option invalide.\n");
+        }
+    }
+}
+
+int randomDisplay() { 
+    srand(time(NULL));
+
+    size_t randomWidth = (rand() % (MAX_WIDTH - MIN_WIDTH + 1)) + MIN_WIDTH;
+    size_t randomHeight = (rand() % (MAX_WIDTH - MIN_WIDTH + 1)) + MIN_WIDTH;
+    Point* points = (Point*)malloc(sizeof(Point) * randomHeight * randomWidth);
+    if (!points) {
+        fprintf(stderr, ERROR_MEMORY);
+        return 1;
+    }
+    Element** forestMatrix = (Element**)malloc(randomHeight * sizeof(Element*));
+    if (!forestMatrix) {
+        fprintf(stderr, ERROR_MEMORY);
+        free(points);
+        return 1;
+    }
+    initializeMatrix(forestMatrix, randomWidth, randomHeight);
+
+    // We don't want to start the fire on an element with degree 0, so we loop until we find one
+    unsigned short randomX, randomY;
+    do {
+        getRandomPosition(&randomX, &randomY, randomWidth, randomHeight);
+    } while (forestMatrix[randomY][randomX].degree == 0);
+
+    // TODO: We need to display this first time to see the first step of the fire, change to only call one function
     displayMatrix(forestMatrix, randomWidth, randomHeight);
-    setFire(points[i].x, points[i].y, randomWidth, randomHeight, forestMatrix, points, &pointIndex);
-  }
-  free(points);
-  freeMatrix(forestMatrix, randomHeight);
-  return 0;
+
+    // we set the fire on the random position
+    initFire(randomX, randomY, forestMatrix);
+
+    // TODO: Same as above, we need to display this first time to see the first step of the fire
+    displayMatrix(forestMatrix, randomWidth, randomHeight);
+
+    setFire(randomX, randomY, randomWidth, randomHeight, forestMatrix, points, &pointIndex);
+
+    // For each point in the array, we set the fire on the point and we add the new points to the array
+    for (size_t i = 0; i < pointIndex; i++) {
+        displayMatrix(forestMatrix, randomWidth, randomHeight);
+        setFire(points[i].x, points[i].y, randomWidth, randomHeight, forestMatrix, points, &pointIndex);
+
+        // Display matrix and ask user if they want to continue
+        if (!askToContinue()) {
+            break;
+        }
+    }
+// Ajouter l'appel à la fonction revoirEtapes ici
+    revoirEtapes(forestMatrix, randomWidth, randomHeight);
+    free(points);
+    freeMatrix(forestMatrix, randomHeight);
+    // Indicate that the process is finished
+    printf("Simulation terminee. Et en %ld etape\n", (step-1));
+    return 0;
 }
---
+++ menu/heading_menu.c
@@ -0,0 +1,55 @@
+/**
+ * @file heading_menu.c
+ * @brief The set of functions used for the menu
+ * @version 1.0 01/09/2023
+ * Projet C - ESIEA
+*/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <time.h>
+//#include "heading_menu.h"
+#include "../display/random/random_display.h"
+
+
+// Allows you to display the menu before the start of the game
+void menu() {
+    int gameMode;
+    int quit = 0;
+    printf("Bienvenue dans l'emulation d'incendie en milieu forestier\n");
+    while (!quit) { 
+        printf("Veuillez choisir le mode de jeu que vous souhaitez ? \n");
+        printf("\t1 - Charger une carte\n");
+        printf("\t2 - Generer aleatoirement une carte\n");
+        printf("\t3 - Quitter\n");
+        // Read user choice
+        scanf("%d", &gameMode);
+        switch (gameMode) {
+            case 1:
+                // Sacha's code here
+                break; 
+            case 2:
+                randomDisplay();
+                break;
+            case 3:
+                quit = 1; 
+                break; 
+            default:
+                printf("Choix invalide. Veuillez choisir un mode de jeu valide.\n");
+        }
+    }
+}
+
+// Allows you to leave the game with player confirmation
+void leave() {
+    char giveUp[2];
+    printf("Etes-vous sur de vouloir quitter la partie ? (y/n) ");
+    scanf("%1s", giveUp); 
+    if (tolower(giveUp[0]) == 'y') {
+        exit(0);
+    } else {
+        menu();
+    }
+}
