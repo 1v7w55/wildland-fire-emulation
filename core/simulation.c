e3a6c00f6d21abf460a22fd49ab41906b3a9641c
sacha <sachacmoi77@gmail.com>
2023-12-19 21:43:17 +0100

    ajout du compteur

--- core/simulation.c
+++ core/simulation.c
@@ -18,6 +18,10 @@
  * and then prints a random symbol from an array for each row and column of the output.
  */
 
+void displayStep(size_t fireSpreadStep){
+  printf("Etape %ld :\n", fireSpreadStep);
+}
+
 void displayMatrix(Element** matrix, size_t width, size_t height) {
   for (size_t i = 0; i < height; i++) {
     for (size_t j = 0; j < width; j++) {
@@ -137,13 +141,14 @@ void processFireSpread(Element** forestMatrix, size_t width, size_t height, Poin
     currentPointIndex = 0;
     newPointsOnFire = 0;
     size_t numberOfPointsOnFire = *pointIndex;
-
+    
     if (*displayMenu) {
       userChoice = userMenu(forestMatrix, width, height, listPointsOnFire, pointIndex, displayMenu, &forestStack);
 
       if (userChoice == 3) {
         if (forestStack != NULL) {
           pop(&forestMatrix, width, height, listPointsOnFire, pointIndex);
+          displayStep(fireSpreadStep);
           displayMatrix(forestMatrix, width, height);
           hasUserRolledBack = true;
         } else {
@@ -170,6 +175,8 @@ void processFireSpread(Element** forestMatrix, size_t width, size_t height, Poin
         currentPointIndex++;
       }
       checkExtinctAsh(forestMatrix, listPointsOnFire, pointIndex);
+      fireSpreadStep++;
+      displayStep(fireSpreadStep);
       displayMatrix(forestMatrix, width, height);
       push(forestMatrix, width, height, listPointsOnFire, *pointIndex);
     }
@@ -233,8 +240,10 @@ int randomForestCreation() {
   listPointsOnFire[pointIndex].x = randomX;
   listPointsOnFire[pointIndex].y = randomY;
   pointIndex++;
-
+  fireSpreadStep = 0;
+  displayStep(fireSpreadStep);
   displayMatrix(forestMatrix, gridWidth, gridHeight);
+
 
   push(forestMatrix, gridWidth, gridHeight, listPointsOnFire, pointIndex);
 
--- stack/stack.c
+++ stack/stack.c
@@ -47,7 +47,11 @@ void pop(Element ***map, size_t width, size_t height, Point* listPointsOnFire, s
     memcpy(listPointsOnFire, toPop->tabCoordFeu, toPop->nbFlames * sizeof(Point));
     *pointIndex = toPop->nbFlames;
 
-    fireSpreadStep = toPop->iteration;
+	if (fireSpreadStep > 0) {
+		printf("a");
+		fireSpreadStep--;
+		}
+    //fireSpreadStep = toPop->iteration;
 
     free(toPop->adresseCarte);
     free(toPop->tabCoordFeu);
