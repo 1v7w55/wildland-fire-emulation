f65f9c2177f09c50fa727a854d78362825a23616
sacha <sachacmoi77@gmail.com>
2023-12-19 16:22:57 +0100

    ajoute dans le bon dossier

--- utils/save.c
+++ utils/save.c
@@ -1,12 +1,14 @@
 #include "../core/simulation.h"
 #include <stdio.h>
+#include<unistd.h> 
 #define MAX_STR 100
 
 void save_grid(Element** matrix,int height, int width){
     char nomFichier [MAX_STR];
     printf("Nom de la sauvegarede :");
     scanf("%s",nomFichier);
-
+    if (chdir("./saves") != 0) perror("chdir() to /save failed"); 
+        
     FILE *fichier = fopen(nomFichier, "w");
     if (fichier != NULL) {
       fprintf(fichier, "%d %d\n", height, width);
@@ -14,10 +16,12 @@ void save_grid(Element** matrix,int height, int width){
             for (size_t j = 0; j < width; j++) {
             fprintf(fichier, "%c", matrix[i][j].symbol);
             }
-            fprintf(fichier, "\n");
+            if (i != height-1){
+                fprintf(fichier, "\n");
+            }
         }
     fclose(fichier);
-    printf("Le fichier a bien été enregistré dans le dossier save.");
+    printf("Le fichier a bien été enregistré dans le dossier save.\n");
 
     }
     else{
